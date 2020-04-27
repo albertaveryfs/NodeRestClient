5db1f01a5bb644f0300c2dc20557ef8918aa2f40
Albert Avery <albertavery@mail.fresnostate.edu>
2020-04-27 16:23:26 -0700

    initial commit

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { HttpService } from '../../shared-service/http.service';
 
 export interface IBike {
   id?: number;
@@ -20,15 +21,67 @@ export class CartComponent implements OnInit {
 
   bikes: Array<IBike> = [];
   myName = '';
+  students = [];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private http: HttpService
   ) { }
 
   async ngOnInit() {
-
+    await this.refresh();
+    // this.createStudent('student', { name: 'John Doe', email: 'johndoe@example.com'});
+    // this.updateStudent('student/id/3', { name: 'Bob Jones'});
   }
 
+  async refresh() {
+    this.students = await this.getStudents('student');
+  }
 
+  // getStudents('student');
+  async getStudents(path: string) {
+    const resp = await this.http.get(path);
+    console.log('resp from getStudents()', resp);
+    return resp;
+  }
+
+  async createStudent() {
+    const student = {
+      name: null,
+      email: null,
+      phonenum: null
+    };
+    const resp = await this.http.post('student', student);
+    console.log('from createStudent resp: ', resp);
+    if (resp) {
+      // this.refresh();
+      this.students.unshift(resp);
+      this.toastService.showToast('success', 3000, 'Student create success!');
+    } else {
+      this.toastService.showToast('danger', 3000, 'Student create failed!');
+
+    }
+    return resp;
+  }
+
+  async updateStudent(student: any) {
+    console.log('from updateStudent student: ', student)
+    const resp = await this.http.put(`student/id/${student.id}`, student);
+    if (resp) {
+      this.toastService.showToast('success', 3000, 'Student updated successfully!');
+    }
+    return resp;
+  }
+
+  async removeStudent(student: any, index: number) {
+    console.log('from removeStudent...', index);
+    const resp = await this.http.delete(`student/id/${student.id}`);
+    console.log('resp from removeStudent...', resp);
+    if (resp) {
+      this.refresh();
+    } else {
+      this.toastService.showToast('danger', 3000, 'Delete car failed!');
+    }
+  }
 }
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { HttpService } from 'src/shared-service/http.service';
+
 
 @Component({
   selector: 'app-main-nav',
@@ -11,7 +13,8 @@ export class MainNavComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private http: HttpService
   ) { }
 
   ngOnInit() {
@@ -19,6 +22,14 @@ export class MainNavComponent implements OnInit {
 
   showAbout() {
     this.toastService.showToast('success', 7000, 'This application was created by Lucas Phan (C).');
+    this.http.testing();
   }
 
+  async logout() {
+    const resp = await this.http.logout();
+    if (resp.statusCode === 200) {
+      localStorage.removeItem('id_token');
+      this.router.navigate(['login']);
+    }
+  }
 }
